08c397d415d6b9f92205a0ffcda8917332c3a2d8
geraldinesandoval <sandovalgigi96@gmail.com>
2025-08-08 11:26:20 -0600

    Commit creacion de metodos fila destacada y alumno destacado Ariel

--- src/controlador/Metodos.java
+++ src/controlador/Metodos.java
@@ -11,8 +11,9 @@ import modelo.Alumno;
  * @author Ian
  */
 public class Metodos {
-    
+
     public static double calcularFilaPromedio(Alumno[][] alumnos, int fila, int columnas) {
+
         double suma = 0;
         for (int j = 0; j < columnas; j++) {
             suma += alumnos[fila][j].getPromedio();
@@ -30,5 +31,38 @@ public class Metodos {
             }
         }
         return suma / totalEdades;
+    }
+
+    public static int calcularFilaDestacada(Alumno[][] posicion) {
+
+        double promedio = 0;
+        int filaDestacada = 1;
+
+        for (int i = 0; i < posicion.length; i++) {
+
+            double promedioFila = calcularFilaPromedio(posicion, i, posicion[i].length);
+
+            if (promedioFila > promedio) {
+                promedio = promedioFila;
+                filaDestacada = i;
+            }
+        }
+        return filaDestacada;
+    }
+
+    public static Alumno mostrarAlumnoMasDestacado(Alumno[][] alumnos) {
+        Alumno alumDestacado = alumnos[0][0];
+
+        for (Alumno[] fila : alumnos) {
+            for (Alumno a : fila) {
+                if (a.getPromedio() > alumDestacado.getPromedio()) {
+                    alumDestacado = a;
+                }
+            }
+        }
+
+        System.out.println("El alumno más destacado es: " + alumDestacado.getNombre()
+                + " con promedio de " + alumDestacado.getPromedio());
+        return alumDestacado;
     }
 }
--- src/vista/PrincipalVista.java
+++ src/vista/PrincipalVista.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 import modelo.Alumno;
 import controlador.Metodos;
 import static controlador.Metodos.calcularEdad;
+import static controlador.Metodos.calcularFilaDestacada;
 import static controlador.Metodos.calcularFilaPromedio;
+import static controlador.Metodos.mostrarAlumnoMasDestacado;
 
 /**
  *
@@ -17,24 +19,24 @@ import static controlador.Metodos.calcularFilaPromedio;
 public class PrincipalVista {
 
     public static void main(String[] args) {
-       // TODO code application logic here
-       registrarAlumnos();
+        // TODO code application logic here
+        registrarAlumnos();
     }
-    
-    public static void registrarAlumnos(){
-    Scanner sc = new Scanner(System.in);
-       
+
+    public static void registrarAlumnos() {
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Ingrese el numero de filas: ");
         int filas = sc.nextInt();
         System.out.print("Ingrese el numero de columnas: ");
         int columnas = sc.nextInt();
-         sc.nextLine();
-         
-         Alumno[][] posicion = new Alumno[filas][columnas];
-         
-         for (int i = 0; i < filas; i++) {
+        sc.nextLine();
+
+        Alumno[][] posicion = new Alumno[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.println("\nAlumno en posicion ["+ i + "][" + j + "]");
+                System.out.println("\nAlumno en posicion [" + i + "][" + j + "]");
 
                 System.out.print("Nombre: ");
                 String nombre = sc.nextLine();
@@ -51,25 +53,32 @@ public class PrincipalVista {
 
                 System.out.print("Promedio: ");
                 double promedio = sc.nextDouble();
-                sc.nextLine(); 
-       
-       posicion[i][j] = new Alumno (nombre,edad, carrera, cuatrimestre, promedio);
-    
+                sc.nextLine();
+
+                posicion[i][j] = new Alumno(nombre, edad, carrera, cuatrimestre, promedio);
+
             }
-         }
-         System.out.println("\n=== Lista de Alumnos ===");
+        }
+
+        System.out.println("\n=== Lista de Alumnos ===");
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 System.out.print("[" + i + "][" + j + "] -> ");
                 posicion[i][j].mostrarInfo();
             }
+            
             double promedioFila = calcularFilaPromedio(posicion, i, columnas);
-                System.out.println("Promedio de calificaciones en fila " + i + ": " + promedioFila);
+            System.out.println("Promedio de calificaciones en fila " + i + ": " + promedioFila);
         }
         double promedioGrupo = calcularEdad(posicion, filas, columnas);
         System.out.println("Promedio de edades del grupo: " + promedioGrupo);
-    } 
-    
+        
+        int filaDestacada = calcularFilaDestacada(posicion);
+        System.out.println("Fila destacada es:" + filaDestacada);
+        
+        Alumno alumnoDestacado = mostrarAlumnoMasDestacado(posicion);
+        System.out.println("Alumno destacado: " + alumnoDestacado);
+        
     }
-    
 
+}
